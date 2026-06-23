b93275736fc754b191a7a6d1c2e7b4b597550ec0
Kucher <a.romenskyi99@gmail.com>
2026-06-24 00:42:35 +0300

    fixed song dao

--- shared/source/local/src/commonMain/kotlin/ua/kucher/player/local/song/SongDao.kt
+++ shared/source/local/src/commonMain/kotlin/ua/kucher/player/local/song/SongDao.kt
@@ -1,14 +1,11 @@
 package ua.kucher.player.local.song
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
 import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 import ua.kucher.player.local.TableName
-import ua.kucher.player.local.song.entity.MergeSongsResult
 import ua.kucher.player.local.song.entity.SongDto
 import ua.kucher.player.local.song.entity.SongEntity
 import ua.kucher.player.local.song.entity.SongWithArtwork
@@ -18,7 +15,7 @@ internal interface SongDao {
 
 
     @Query("SELECT * FROM ${TableName.SONG_TABLE_NAME}")
-    suspend fun getSongsSnapshot(): List<SongDto>
+    suspend fun getSongsSnapshot(): List<SongEntity>
 
     @Query("SELECT * FROM ${TableName.SONG_TABLE_NAME}")
     fun getSongs(): Flow<List<SongDto>>
@@ -37,12 +34,11 @@ internal interface SongDao {
 
     @Query("UPDATE ${TableName.SONG_TABLE_NAME} SET artwork=:artworkUri WHERE id=:id")
     suspend fun insertArtwork(id: Long, artworkUri: String)
-
     @Upsert
-    suspend fun upsertSong(song: SongEntity)
+    suspend fun upsertSongs(list: List<SongEntity>)
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertSong(song: SongEntity)
+    @Query("DELETE FROM ${TableName.SONG_TABLE_NAME} WHERE id IN (:ids)")
+    suspend fun deleteSongs(ids: List<Long>)
 
     @Transaction
     suspend fun insertArtworks(songsWithArtworks: List<SongWithArtwork>) {
@@ -52,36 +48,13 @@ internal interface SongDao {
     }
 
     @Transaction
-    suspend fun mergeSongs(deviceSongs: List<SongEntity>): MergeSongsResult {
-        val deviceIds = deviceSongs.map(SongEntity::id).toSet()
-        val dbSongs = getSongsSnapshot()
-        val dbSongsById = dbSongs.associateBy { dto ->
-            dto.song.id
-        }
-
-        val removedIds = dbSongsById.keys - deviceIds
-        val insertedSongs = mutableListOf<SongEntity>()
-
-        removedIds.forEach { id ->
-            deleteSongById(id)
-        }
-
-        deviceSongs.forEach { newSong ->
-            val oldSong = dbSongsById[newSong.id]
-            when {
-                oldSong == null -> {
-                    insertSong(newSong)
-                    insertedSongs += newSong
-                }
-
-                oldSong.song.lastModified != newSong.lastModified -> {
-                    upsertSong(newSong)
-                }
-            }
-        }
-        return MergeSongsResult(
-            removedSongIds = removedIds,
-            insertedSongs = insertedSongs
-        )
+    suspend fun mergeSongs(
+        insert: List<SongEntity>,
+        upsert: List<SongEntity>,
+        delete: List<SongEntity>
+    ) {
+        upsertSongs(insert)
+        upsertSongs(upsert)
+        deleteSongs(delete.map { it.id })
     }
 }
--- shared/source/local/src/commonMain/kotlin/ua/kucher/player/local/song/entity/MergeSongsResult.kt
+++
@@ -1,6 +0,0 @@
-package ua.kucher.player.local.song.entity
-
-internal data class MergeSongsResult(
-    val removedSongIds: Set<Long>,
-    val insertedSongs: List<SongEntity>,
-)
